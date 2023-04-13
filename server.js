de8e5e8891d04d5c401478d3170800c46df3c1fd
bobbyrosehag <bobbyrosehag@gmail.com>
2023-04-13 14:12:10 +0200

    Added env and static serve

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+/* --------------------------------- Imports -------------------------------- */
 const ws = require("ws");
-const { WebSocketServer } = ws;
-const { v4: uuid } = require("uuid");
-const server = new WebSocketServer({ port: 5000 });
+const dotenv = require("dotenv").config();
+const express = require("express");
 
+/* -------------------------------- Variables ------------------------------- */
+const { WebSocketServer } = ws;
+const CONFIG = {
+  EXPRESS_PORT: process.env.EXPRESS_PORT || 3000,
+  WSS_PORT: process.env.WSS_PORT || 5000,
+};
+const { v4: uuid } = require("uuid");
+const server = new WebSocketServer({ port: CONFIG.WSS_PORT });
+const app = express();
+
+/* ------------------------------- Middleware ------------------------------- */
+app.use(express.static("public"));
+
+/* -------------------------------- Listeners ------------------------------- */
 server.on("connection", (socket) => {
   socket.id = uuid();
   socket.on("message", (data) => {
@@ -12,3 +26,7 @@ server.on("connection", (socket) => {
     });
   });
 });
+
+app.listen(CONFIG.EXPRESS_PORT, () =>
+  console.log("Server running on port: ", CONFIG.EXPRESS_PORT)
+);
